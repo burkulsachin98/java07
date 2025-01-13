ef5d77aa8d75ea946dfb16cc06ca1520b9a8298d
burkulsachin <sachinburkul2006@gmail.com>
2025-01-13 17:59:37 +0530

    java pp

--- src/logical/programs/Star3.java
+++ src/logical/programs/Star3.java
@@ -1,16 +1,24 @@
 package logical.programs;
 
 public class Star3 {
-	
-	
+
+
 	public static void main(String[] args) {
-		
-		for (int i=0;i<=4;i++) {
-			for(int j=4;j<=i;j--) {
-				
+
+		for (int i = 0; i <= 4; i++)  {
+			for (int j = 4; j > i; j--) {
+				System.out.print(" ");
+
 			}
+			for (int k = 0; k <= i; k++){
+				System.out.print("* ");
+			}
+			System.out.println();
+
 		}
-}
+
+	}
+
 }
 
 
